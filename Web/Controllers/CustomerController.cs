546440f9d788eaa69e82f2c2b83dc57efc227c2c
kutluhanyegit-linux <kutluhanyegit@icloud.com>
2022-11-30 23:14:23 +0300

    V4

--- Web/Controllers/CustomerController.cs
+++ Web/Controllers/CustomerController.cs
@@ -23,6 +23,12 @@ namespace Web.Controllers
         {
             return View();
         }
+        [Route("musteri-ekle")]
+        public IActionResult Add()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
